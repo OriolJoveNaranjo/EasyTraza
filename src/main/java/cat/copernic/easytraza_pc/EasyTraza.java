6635a7693a9001be276ebf31910aa363607bb8e5
Oriol <jove.narnjo.oriol@alumnat.copernic.cat>
2026-04-10 12:00:04 +0200

    Connexió base de dades

---
+++ src/main/java/cat/copernic/easytraza_pc/EasyTraza.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package cat.copernic.easytraza_pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author orjon
+ */
+@SpringBootApplication
+public class EasyTraza {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyTraza.class, args);
+    }
+}
