3ae1581c794a205efdf9cf32bafdea15db11aba8
Oriol <jove.narnjo.oriol@alumnat.copernic.cat>
2026-05-10 21:10:27 +0200

    funcionalitat de loguin i logout

---
+++ backend/src/main/java/cat/copernic/easytraza/controller/PanellController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.copernic.easytraza.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author orjon
+ * Controlador del panel principal después del login.
+ */
+@Controller
+public class PanellController {
+
+    @GetMapping("/panell")
+    public String panell() {
+        return "panell";
+    }
+}
