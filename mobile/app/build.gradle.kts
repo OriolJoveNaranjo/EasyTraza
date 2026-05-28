d958d873391ac8b6a6f39d51af4657b875f3930d
Oriol <jove.narnjo.oriol@alumnat.copernic.cat>
2026-05-28 21:41:23 +0200

    Canvi de logo de la app

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -17,11 +17,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        externalNativeBuild {
-            cmake {
-                cppFlags += ""
-            }
-        }
+
     }
 
     buildTypes {
@@ -40,12 +36,7 @@ android {
     buildFeatures {
         compose = true
     }
-    externalNativeBuild {
-        cmake {
-            path = file("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
-        }
-    }
+    
 }
 
 dependencies {
